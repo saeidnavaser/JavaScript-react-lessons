5c91839e8f9d0ce178768505ca07de832cb8c8f6
saeid navaser <saeidnavaser@gmx.at>
2019-04-01 16:03:50 +0200

    new javascript lesson

---
+++ lesson-2/index.js
@@ -0,0 +1,14 @@
+/*document.write("Saeid Navaser");
+//alert("Saeid Navaser");
+
+function myClick(){
+    alert("Saeid Navaser");
+}
+*/
+
+var number =2;
+var name = "Saeid";
+var num2 = 4;
+var result = number + num2;
+
+document.write(result);
